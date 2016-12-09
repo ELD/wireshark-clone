71d45d23d13b831cf4ddc0c8bbe3e5cb03cf43e6
Eric Dattore <edattore@gmail.com>
2016-12-09 01:13:42 -0700

    finish project

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,27 @@ extern crate pcap;
 mod packet_types;
 
 use std::env;
+use std::collections::HashSet;
 use pcap::Capture;
-use packet_types::PacketTypes;
+use packet_types::{LinkLayerType, TransportLayerType, NetworkLayerType, PacketTypes};
 
 fn main() {
+    let default_packet_types = PacketTypes {
+        link_layer_type: LinkLayerType::Ethernet2,
+        network_layer_type: NetworkLayerType::Other,
+        transport_layer_type: TransportLayerType::Other,
+        packet_length: 0,
+        source_mac: [0; 6],
+        dest_mac: [0; 6],
+        source_ip: [0; 16],
+        dest_ip: [0; 16],
+        source_port: 0,
+        dest_port: 0,
+        is_syn: false,
+        is_fin: false,
+        is_frag: false,
+    };
+
     let cap_file_name = env::args().nth(1).expect("Expected a command line parameter here");
 
     let mut capture = Capture::from_file(cap_file_name).ok().expect("Expected a valid capture file");
@@ -16,10 +33,144 @@ fn main() {
         packets.push(PacketTypes::new(packet.data, packet.header.len));
     }
 
-    for packet in packets.iter() {
-        packet.print_link_layer_type();
-        packet.print_network_layer_type();
+    // Link Layer
+    let num_ethernet = packets.iter().filter(|&e| e.link_layer_type == LinkLayerType::Ethernet2).count();
+    let num_802_3 = packets.iter().filter(|&e| e.link_layer_type == LinkLayerType::Novell8023).count();
+
+    println!("Link Layer:");
+    println!("Number of Ethernet II frames: {}", num_ethernet);
+    println!("Number of Novell 802.3 frames: {}", num_802_3);
+    println!();
+
+    // Network Layer
+    let num_arp = packets.iter().filter(|&e| e.network_layer_type == NetworkLayerType::ARP).count() as u32;
+    let arp_total: u32 = packets.iter().filter(|&e| e.network_layer_type == NetworkLayerType::ARP).fold(0, |acc, ref e| acc + e.packet_length);
+    let max_arp = packets.iter().filter(|&e| e.network_layer_type == NetworkLayerType::ARP).max().unwrap_or(&default_packet_types).packet_length;
+    let min_arp = packets.iter().filter(|&e| e.network_layer_type == NetworkLayerType::ARP).min().unwrap_or(&default_packet_types).packet_length;
+    let mut avg_arp = 0;
+    if num_arp != 0 {
+        avg_arp = arp_total / num_arp
     }
 
-    println!("Number of packets processed: {}", packets.len());
+    let num_ipv4 = packets.iter().filter(|&e| e.network_layer_type == NetworkLayerType::IPv4).count() as u32;
+    let ipv4_total = packets.iter().filter(|&e| e.network_layer_type == NetworkLayerType::IPv4).fold(0, |acc, ref e| acc + e.packet_length);
+    let max_ipv4 = packets.iter().filter(|&e| e.network_layer_type == NetworkLayerType::IPv4).max().unwrap_or(&default_packet_types).packet_length;
+    let min_ipv4 = packets.iter().filter(|&e| e.network_layer_type == NetworkLayerType::IPv4).min().unwrap_or(&default_packet_types).packet_length;
+    let mut avg_ipv4 = 0;
+    if num_ipv4 != 0 {
+        avg_ipv4 = ipv4_total / num_ipv4
+    }
+
+    let num_ipv6 = packets.iter().filter(|&e| e.network_layer_type == NetworkLayerType::IPv6).count() as u32;
+    let ipv6_total = packets.iter().filter(|&e| e.network_layer_type == NetworkLayerType::IPv6).fold(0, |acc, ref e| acc + e.packet_length);
+    let max_ipv6 = packets.iter().filter(|&e| e.network_layer_type == NetworkLayerType::IPv6).max().unwrap_or(&default_packet_types).packet_length;
+    let min_ipv6 = packets.iter().filter(|&e| e.network_layer_type == NetworkLayerType::IPv6).min().unwrap_or(&default_packet_types).packet_length;
+    let mut avg_ipv6 = 0;
+    if num_ipv6 != 0 {
+        avg_ipv6 = ipv6_total / num_ipv6
+    }
+
+    let num_other = packets.iter().filter(|&e| e.network_layer_type == NetworkLayerType::Other).count();
+
+    println!("Network Layer:");
+    println!("Number of ARP packets: {}", num_arp);
+    println!("Max ARP packet size: {}, min ARP packet size: {}, average ARP packet size: {}", max_arp, min_arp, avg_arp);
+
+    println!("Number of IPv4 packets: {}", num_ipv4);
+    println!("Max IPv4 packet size: {}, min IPv4 packet size: {}, average IPv4 packet size: {}", max_ipv4, min_ipv4, avg_ipv4);
+
+    println!("Number of IPv6 packets: {}", num_ipv6);
+    println!("Max IPv6 packet size: {}, min IPv6 packet size: {}, average IPv6 packet size: {}", max_ipv6, min_ipv6, avg_ipv6);
+
+    println!("Number of other packets: {}", num_other);
+
+    println!();
+
+    // Transport Layer
+    let num_icmp = packets.iter().filter(|&e| e.transport_layer_type == TransportLayerType::ICMP).count() as u32;
+    let icmp_total = packets.iter().filter(|&e| e.transport_layer_type == TransportLayerType::ICMP).fold(0, |acc, ref e| acc + e.packet_length);
+    let max_icmp = packets.iter().filter(|&e| e.transport_layer_type == TransportLayerType::ICMP).max().unwrap_or(&default_packet_types).packet_length;
+    let min_icmp = packets.iter().filter(|&e| e.transport_layer_type == TransportLayerType::ICMP).min().unwrap_or(&default_packet_types).packet_length;
+    let mut avg_icmp = 0;
+    if num_icmp != 0 {
+        avg_icmp = icmp_total / num_icmp
+    }
+
+    let num_tcp = packets.iter().filter(|&e| e.transport_layer_type == TransportLayerType::TCP).count() as u32;
+    let tcp_total = packets.iter().filter(|&e| e.transport_layer_type == TransportLayerType::TCP).fold(0, |acc, ref e| acc + e.packet_length);
+    let max_tcp = packets.iter().filter(|&e| e.transport_layer_type == TransportLayerType::TCP).max().unwrap_or(&default_packet_types).packet_length;
+    let min_tcp = packets.iter().filter(|&e| e.transport_layer_type == TransportLayerType::TCP).min().unwrap_or(&default_packet_types).packet_length;
+    let mut avg_tcp = 0;
+    if num_tcp != 0 {
+        avg_tcp = tcp_total / num_tcp
+    }
+
+    let num_udp = packets.iter().filter(|&e| e.transport_layer_type == TransportLayerType::UDP).count() as u32;
+    let udp_total = packets.iter().filter(|&e| e.transport_layer_type == TransportLayerType::UDP).fold(0, |acc, ref e| acc + e.packet_length);
+    let max_udp = packets.iter().filter(|&e| e.transport_layer_type == TransportLayerType::UDP).max().unwrap_or(&default_packet_types).packet_length;
+    let min_udp = packets.iter().filter(|&e| e.transport_layer_type == TransportLayerType::UDP).min().unwrap_or(&default_packet_types).packet_length;
+    let mut avg_udp = 0;
+    if num_udp != 0 {
+        avg_udp = udp_total / num_udp
+    }
+
+    let num_other = packets.iter().filter(|&e| e.transport_layer_type == TransportLayerType::Other).count();
+
+    println!("Transport Layer:");
+
+    println!("Number of ICMP packets: {}", num_icmp);
+    println!("Max ICMP packet size: {}, min ICMP packet size: {}, average ICMP packet size: {}", max_icmp, min_icmp, avg_icmp);
+
+    println!("Number of TCP packets: {}", num_tcp);
+    println!("Max TCP packet size: {}, min TCP packet size: {}, average TCP packet size: {}", max_tcp, min_tcp, avg_tcp);
+
+    println!("Number of UDP packets: {}", num_udp);
+    println!("Max UDP packet size: {}, min UDP packet size: {}, average UDP packet size: {}", max_udp, min_udp, avg_udp);
+
+    println!("Number of other packets: {}", num_other);
+    println!();
+
+    // MAC Addresses
+    let mut mac_address: HashSet<[u8; 6]> = HashSet::new();
+    let mut ip_address: HashSet<[u8; 16]> = HashSet::new();
+    let mut udp_ports: HashSet<u16> = HashSet::new();
+    let mut tcp_ports: HashSet<u16> = HashSet::new();
+    let empty_ip = [0; 16];
+    let mut syn_and_fin = 0;
+    let mut frag_count = 0;
+
+    for packet in packets {
+        mac_address.insert(packet.source_mac);
+        mac_address.insert(packet.dest_mac);
+
+        ip_address.insert(packet.source_ip);
+        ip_address.insert(packet.dest_ip);
+
+        match packet.transport_layer_type {
+            TransportLayerType::TCP => {
+                tcp_ports.insert(packet.source_port);
+                tcp_ports.insert(packet.dest_port);
+            },
+            TransportLayerType::UDP => {
+                udp_ports.insert(packet.source_port);
+                udp_ports.insert(packet.dest_port);
+            },
+            _ => {},
+        }
+
+        if packet.is_syn || packet.is_fin {
+            syn_and_fin += 1;
+        }
+
+        if packet.is_frag {
+            frag_count += 1;
+        }
+    }
+
+    println!("Unique MAC addresses: {}", mac_address.len());
+    println!("Unique IP addresses: {}", ip_address.iter().filter(|&e| e != &empty_ip).count());
+    println!("Unique UDP port numbers: {}", udp_ports.iter().filter(|&e| *e != 0).count());
+    println!("Unique TCP port numbers: {}", tcp_ports.iter().filter(|&e| *e != 0).count());
+    println!("Number of SYN and FIN packets: {}", syn_and_fin);
+    println!("Number of fragmented packets: {}", frag_count);
 }
--- src/packet_types.rs
+++ src/packet_types.rs
@@ -1,8 +1,12 @@
+use std::cmp::{Ordering};
+
+#[derive(PartialEq, Eq)]
 pub enum LinkLayerType {
     Novell8023,
     Ethernet2,
 }
 
+#[derive(PartialEq, Eq)]
 pub enum NetworkLayerType {
     ARP,
     IPv4,
@@ -10,6 +14,7 @@ pub enum NetworkLayerType {
     Other,
 }
 
+#[derive(PartialEq, Eq)]
 pub enum TransportLayerType {
     ICMP,
     TCP,
@@ -17,11 +22,21 @@ pub enum TransportLayerType {
     Other,
 }
 
+#[derive(PartialEq, Eq)]
 pub struct PacketTypes {
-    link_layer_type: LinkLayerType,
-    network_layer_type: NetworkLayerType,
-    transport_layer_type: TransportLayerType,
-    packet_length: u32,
+    pub link_layer_type: LinkLayerType,
+    pub network_layer_type: NetworkLayerType,
+    pub transport_layer_type: TransportLayerType,
+    pub packet_length: u32,
+    pub source_mac: [u8; 6],
+    pub dest_mac: [u8; 6],
+    pub source_ip: [u8; 16],
+    pub dest_ip: [u8; 16],
+    pub source_port: u16,
+    pub dest_port: u16,
+    pub is_syn: bool,
+    pub is_fin: bool,
+    pub is_frag: bool,
 }
 
 impl PacketTypes {
@@ -29,6 +44,13 @@ impl PacketTypes {
         let mut frame_type = LinkLayerType::Novell8023;
         let mut network_type = NetworkLayerType::Other;
         let mut transport_type = TransportLayerType::Other;
+        let mut source_ip: [u8; 16] = [0; 16];
+        let mut dest_ip: [u8; 16] = [0; 16];
+        let mut source_port = 0;
+        let mut dest_port = 0;
+        let mut is_syn = false;
+        let mut is_fin = false;
+        let mut is_frag = false;
 
         let ethertype = ((data[12] as u16) << 8) | (data[13] as u16);
         if ethertype >= 1536 {
@@ -36,10 +58,68 @@ impl PacketTypes {
 
             if ethertype == 0x0800 {
                 network_type = NetworkLayerType::IPv4;
+                source_ip[0] = data[26];
+                source_ip[1] = data[27];
+                source_ip[2] = data[28];
+                source_ip[3] = data[29];
+
+                dest_ip[0] = data[30];
+                dest_ip[1] = data[31];
+                dest_ip[2] = data[32];
+                dest_ip[3] = data[33];
             } else if ethertype == 0x0806 {
                 network_type = NetworkLayerType::ARP;
+                source_ip[0] = data[29];
+                source_ip[1] = data[30];
+                source_ip[2] = data[31];
+                source_ip[3] = data[32];
+
+                dest_ip[0] = data[38];
+                dest_ip[1] = data[39];
+                dest_ip[2] = data[40];
+                dest_ip[3] = data[41];
             } else if ethertype == 0x86DD {
                 network_type = NetworkLayerType::IPv6;
+                source_ip.clone_from_slice(&data[21..37]);
+                dest_ip.clone_from_slice(&data[38..54]);
+            }
+
+            match network_type {
+                NetworkLayerType::IPv4 => {
+                    let protocol_code = data[23];
+                    transport_type = get_trans_type(protocol_code);
+
+                    let frag_bits = (data[20] << 2 as u8) >> 7;
+                    if frag_bits == 1 {
+                        is_frag = true;
+                    }
+                },
+                NetworkLayerType::IPv6 => {
+                    let protocol_code = data[20];
+                    transport_type = get_trans_type(protocol_code);
+                },
+                _ => transport_type = TransportLayerType::Other,
+            }
+
+            match transport_type {
+                TransportLayerType::TCP => {
+                    source_port = ((data[34] as u16) << 8) | (data[35] as u16);
+                    dest_port = ((data[36] as u16) << 8) | (data[37] as u16);
+                    let fin_bit = (data[47] << 7 as u8) >> 7;
+                    let syn_bit = (data[47] << 6 as u8) >> 7;
+                    if fin_bit == 1 {
+                        is_fin = true;
+                    }
+
+                    if syn_bit == 1 {
+                        is_syn = true;
+                    }
+                },
+                TransportLayerType::UDP => {
+                    source_port = ((data[34] as u16) << 8) | (data[35] as u16);
+                    dest_port = ((data[36] as u16) << 8) | (data[37] as u16);
+                },
+                _ => {},
             }
         }
 
@@ -48,30 +128,42 @@ impl PacketTypes {
             network_layer_type: network_type,
             transport_layer_type: transport_type,
             packet_length: len,
+            source_mac: [data[0], data[1], data[2], data[3], data[4], data[5]],
+            dest_mac: [data[6], data[7], data[8], data[9], data[10], data[11]],
+            source_ip: source_ip,
+            dest_ip: dest_ip,
+            source_port: source_port,
+            dest_port: dest_port,
+            is_syn: is_syn,
+            is_fin: is_fin,
+            is_frag: is_frag,
         }
     }
+}
 
-    pub fn print_link_layer_type(&self) {
-        let string_type: &'static str;
-
-        match self.link_layer_type {
-            LinkLayerType::Ethernet2 => string_type = "Ethernet II",
-            LinkLayerType::Novell8023 => string_type = "Novell 802.3",
-        }
-
-        println!("Packet's link layer type: {}", string_type);
+impl PartialOrd for PacketTypes {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
+}
 
-    pub fn print_network_layer_type(&self) {
-        let string_type: &'static str;
-
-        match self.network_layer_type {
-            NetworkLayerType::IPv4 => string_type = "IPv4",
-            NetworkLayerType::IPv6 => string_type = "IPv6",
-            NetworkLayerType::ARP => string_type = "ARP",
-            NetworkLayerType::Other => string_type = "Other",
+impl Ord for PacketTypes {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.packet_length < other.packet_length {
+            return Ordering::Less
+        } else if self.packet_length > other.packet_length {
+            return Ordering::Greater
+        } else {
+            return Ordering::Equal
         }
+    }
+}
 
-        println!("Network layer type: {}", string_type);
+fn get_trans_type(protocol_code: u8) -> TransportLayerType {
+    match protocol_code {
+        1 => return TransportLayerType::ICMP,
+        6 => TransportLayerType::TCP,
+        17 => TransportLayerType::UDP,
+        _ => return TransportLayerType::Other,
     }
 }
